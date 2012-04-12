ab94c2abd224a3723bb478b2dd7d5b142e4f99c2
ide user ide_onestoptravelsg <onestoptravel.sg@gmail.com>
2012-04-12 17:56:07 +0000

    make twitter stream readable

--- client/assets/js/onestop.js
+++ client/assets/js/onestop.js
@@ -37,7 +37,7 @@ var tweet_list = $("#tweets");
 function load_tweets() {  
     $.getJSON("/stream", function(tweets) {  
         $.each(tweets, function() {  
-            $("<li>").html(this.text).prependTo(tweet_list);  
+            $("<li></li>").html(this.text).prependTo(tweet_list);  
         });  
         load_tweets();  
     });  
