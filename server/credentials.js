33d161372e7cfc663572ff059eb33ed9b27063d2
ide user ide_onestoptravelsg <onestoptravel.sg@gmail.com>
2012-04-12 17:34:35 +0000

    credentials

---
+++ server/credentials.js
@@ -0,0 +1,8 @@
+var credentials = {
+    consumer_key: 'YWWp6c6nqgZPHqQasQEO2w',
+    consumer_secret: 'nLTwJ34GVTGJ6EZCavkGAY6ieQ0lqZBwa53Ajvv0',
+    access_token_key: '502671281-PjFBG5LWsERNAkImcAWq2gyYfVa3eBpM5O3CS0Nz',
+    access_token_secret: 'iBVK62nTTNn04visU4VCRLroHSI50R9xKyW9Q3uM'
+};
+
+module.exports = credentials;
